d4bbef4e91882c9b0ce5d6f0b187c74d21577d5f
ARTHI B <arthisathya2004@gmail.com>
2026-07-08 01:58:06 +0530

    Connect frontend to Railway backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://movie-booking-system-production.up.railway.app/api',
 });
 
 API.interceptors.request.use((config) => {
